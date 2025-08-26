43a7566bb2f23a129cf3de76febfcf9a1577587b
venumuthusamy <venumuthusamy@gmail.com>
2025-08-26 15:01:42 +0530

    url

--- FinanceApi/Program.cs
+++ FinanceApi/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
