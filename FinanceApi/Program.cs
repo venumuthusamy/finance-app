532df2285a1362ee75b20c5c54843be70c74fec0
venumuthusamy <venumuthusamy@gmail.com>
2025-09-01 14:02:17 +0530

    launch issue

--- FinanceApi/Program.cs
+++ FinanceApi/Program.cs
@@ -9,7 +9,7 @@ using Azure.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Add DbContext
+//Add DbContext 
 
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
